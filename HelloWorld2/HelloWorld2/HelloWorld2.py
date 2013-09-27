f404f8aea60b945508880be7323d983063f34966
sarroutbi <sarroutbi@gmail.com>
2013-09-27 20:01:54 +0200

    Test commit

---
+++ HelloWorld2/HelloWorld2/HelloWorld2.py
@@ -0,0 +1,13 @@
+'''
+Created on Sep 27, 2013
+
+@author: sarroutbi
+'''
+from HelloWorld2Module import HelloWorld
+
+def main():
+    hello = HelloWorld()
+    hello.Print()
+    
+if __name__ == '__main__':
+    main()
---
+++ HelloWorld2/HelloWorld2/HelloWorld2Module.py
@@ -0,0 +1,18 @@
+'''
+Created on Sep 27, 2013
+
+@author: sarroutbi
+'''
+
+class HelloWorld(object):
+    '''
+    classdocs
+    '''
+    def Print(self):
+        print "Hello World"
+        
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
